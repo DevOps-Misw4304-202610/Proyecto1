6e455f8c033f4e7d8b6ca01e55f82747b4b98e84
Juancabel <jc.beltrang1@uniandes.edu.co>
2026-05-09 22:28:56 -0500

    Fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,4 +24,4 @@ while [ "$attempt" -le "$max_attempts" ]; do
 done
 
 echo "Starting Gunicorn..."
-exec gunicorn --bind 0.0.0.0:${PORT:-8000} --workers ${GUNICORN_WORKERS:-4} application:application
+exec gunicorn --bind 0.0.0.0:${PORT:-5000} --workers ${GUNICORN_WORKERS:-4} application:application
